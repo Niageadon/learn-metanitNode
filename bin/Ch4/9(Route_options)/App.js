cf3b1be0580b8cb739d470d533c6096cf5b8a850
Niageadon <legoog.tsop@gmail.com>
2019-08-04 08:19:05 +0300

    ch4 lesson 9 done

---
+++ bin/Ch4/9(Route_options)/App.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/categories/:categoryId/products/:productId", function (request, response) {
+    let catId = request.params["categoryId"];
+    let prodId = request.params["productId"];
+    response.send(`Категория: ${catId}  Товар: ${prodId}`);
+});
+app.listen(3000);
