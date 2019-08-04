1bc6cb675fc8a4a3efc11c23e165baa9e8d357ba
Niageadon <legoog.tsop@gmail.com>
2019-08-04 16:50:35 +0300

    ch4 lesson 19 done

---
+++ bin/Ch4/14(Layout_in_Handlebars)/App.js
@@ -0,0 +1,32 @@
+// TODO:
+const express = require("express");
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
+const app = express();
+
+// устанавливаем настройки для файлов layout
+app.engine("hbs", expressHbs(
+    {
+        layoutsDir: `${__dirname}/views/layouts`,
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use("/contact", function(request, response){
+console.log(`/views/layouts`)
+    response.render("contact", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890"
+    });
+});
+
+app.use("/", function(request, response){
+
+    response.render(__dirname+ "/views/home.hbs");
+});
+app.listen(3000);
